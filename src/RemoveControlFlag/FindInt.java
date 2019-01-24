b3984490b50fadb545e98ea9467a20f8c2016776
aruyamam <aruyamam@gmail.com>
2019-01-25 08:11:37 +0900

    breakを使った制御フラグの削除

---
+++ src/RemoveControlFlag/FindInt.java
@@ -0,0 +1,17 @@
+package RemoveControlFlag;
+
+public class FindInt {
+
+    public static boolean find(int[] data, int target) {
+        boolean found = false;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
+}
