ba692d8a456253925a9f8fa7745f1ed51cb4b90a
aruyamam <aruyamam@gmail.com>
2019-01-24 17:05:37 +0900

    クラスによるマジックナンバーの置き換え

---
+++ src/ReplaceMagicNumberWithSymbolicConstant/RobotCommand.java
@@ -0,0 +1,14 @@
+package ReplaceMagicNumberWithSymbolicConstant;
+
+public class RobotCommand {
+
+    private final String _name;
+
+    public RobotCommand(String name) {
+        _name = name;
+    }
+
+    public String toString() {
+        return "[ RobotCommand: " + _name + " ]";
+    }
+}
