b5a45038482459127f26bfc98d5d0c4446e8455b
aruyamam <aruyamam@gmail.com>
2019-02-02 17:23:52 +0900

    サブクラスによるタイプコードの置き換え

---
+++ src/ReplaceTypeCodeWithSubclasses/Main.java
@@ -0,0 +1,18 @@
+package ReplaceTypeCodeWithSubclasses;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape line = Shape.createShpeLine(0, 0, 100, 200);
+        Shape rectangle = Shape.createShapeRectangle(10, 20, 30, 40);
+        Shape oval = Shape.createShapeOval(100, 200, 300, 400);
+
+        Shape[] shape = {
+            line, rectangle, oval
+        };
+
+        for (Shape s : shape) {
+            s.draw();
+        }
+    }
+}
---
+++ src/ReplaceTypeCodeWithSubclasses/ShapeRectangle.java
@@ -0,0 +1,22 @@
+package ReplaceTypeCodeWithSubclasses;
+
+public class ShapeRectangle extends Shape {
+
+    public ShapeRectangle(int startx, int starty, int endx, int endy) {
+        super(startx, starty, endx, endy);
+    }
+
+    @Override
+    public String getName() {
+        return "RECTANGLE";
+    }
+
+    @Override
+    public void draw() {
+        drawRectangle();
+    }
+
+    private void drawRectangle() {
+        System.out.println("drawRectangle: " + this.toString());
+    }
+}
